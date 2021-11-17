7fa1993ee78c9be34f67b722d6edfdfb9c7eb70b
Huang-Bo <pioneer0221@outlook.com>
2021-11-16 18:30:22 -0800

    API - Streamer login complete

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,8 +7,9 @@ const config = require('../../config/default');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
+const Streamer = require('../../models/Streamer');
 
-// @route    POST api/auth
+// @route    POST api/auth/user
 // @desc     Authenticate user & get token
 // @access   Public
 router.post(
@@ -60,6 +61,60 @@ router.post(
       res.status(500).send('Server error');
     }
   }
+);
+
+// @route    POST api/auth/streamer
+// @desc     Authenticate streamer & get token
+// @access   Public
+router.post(
+    '/streamer/',
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let streamer = await Streamer.findOne({ email });
+  
+        if (!streamer) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const isMatch = await bcrypt.compare(password, streamer.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const payload = {
+          streamer: {
+            id: streamer.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          config.jwtSecret,
+          { expiresIn: '5 days' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
 );
 
 module.exports = router;
