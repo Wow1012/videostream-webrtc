4b103553c1ce310af0c8221d6e76e1ad80e9a0c7
Huang-Bo <pioneer0221@outlook.com>
2021-12-09 11:45:56 -0800

    https

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+// const express = require("express");
+// const connectDB = require("./config/db");
+// const path = require("path");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+
+// // Connect Database
+// connectDB();
+
+// // Init Middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Define Routes
+// app.use("/api/users", require("./routes/api/users"));
+// app.use("/api/streamers", require("./routes/api/streamers"));
+// app.use("/api/auth", require("./routes/api/auth"));
+// app.use("/api/upload", require("./routes/api/upload"));
+// app.use("/api/broadcasters", require("./routes/api/broadcasters"));
+// app.use("/api/setting", require("./routes/api/setting"));
+
+// //SERVING IMAGES
+// app.use("/logo", express.static("logo"));
+// app.use("/images", express.static("images"));
+
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+// const Broadcaster = require("./models/Broadcaster");
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, async () => {
+//   console.log(`Server started on port ${PORT}`);
+//   await Broadcaster.deleteMany();
+// });
+
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
@@ -11,38 +59,46 @@ connectDB();
 
 // Init Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/streamers', require('./routes/api/streamers'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/upload', require('./routes/api/upload'));
-app.use('/api/broadcasters', require('./routes/api/broadcasters'));
-app.use('/api/setting', require('./routes/api/setting'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/streamers", require("./routes/api/streamers"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/upload", require("./routes/api/upload"));
+app.use("/api/broadcasters", require("./routes/api/broadcasters"));
+app.use("/api/setting", require("./routes/api/setting"));
 
 //SERVING IMAGES
-app.use('/logo', express.static('logo'));
-app.use('/images', express.static('images'));
+app.use("/logo", express.static("logo"));
+app.use("/images", express.static("images"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-
-const Broadcaster = require('./models/Broadcaster');
-
-
+const Broadcaster = require("./models/Broadcaster");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+const options = {
+  key: fs.readFileSync("HTTPS_Permissions/key.pem"),
+  cert: fs.readFileSync("HTTPS_Permissions/cert.pem"),
+};
+
+const server = https.createServer(options, app);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+server.listen(PORT, async () => {
   console.log(`Server started on port ${PORT}`);
   await Broadcaster.deleteMany();
 });
--- server_socket.js
+++ server_socket.js
@@ -1,80 +1,93 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const socket = require('socket.io')
-const io = socket(server)
-const cors = require("cors")
-const { addUser, getUser, deleteUser, getUsers } = require('./user')
+const express = require("express");
+const https = require("https");
+const app = express();
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("HTTPS_Permissions/key.pem"),
+  cert: fs.readFileSync("HTTPS_Permissions/cert.pem"),
+};
+
+const server = https.createServer(options, app);
+const socket = require("socket.io");
+const io = socket(server);
+const cors = require("cors");
+const { addUser, getUser, deleteUser, getUsers } = require("./user");
 
 app.use(cors());
 
 let broadcasters = {};
 let broadcaster;
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+  console.log("CLIENT CONNECTED", socket.id);
 
-    console.log("CLIENT CONNECTED", socket.id);
+  //VIDEO BROADCATING
+  socket.on("broadcaster", ({ room }) => {
+    broadcasters[room] = socket.id;
+    console.log(broadcasters, room, socket.id);
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+    console.log(`Broadcaster ${broadcasters[room]} Connect`);
+  });
 
-    //VIDEO BROADCATING
-    socket.on("broadcaster", ({room}) => {
-        broadcasters[room]=socket.id;
-        console.log(broadcasters, room, socket.id,);
-        broadcaster = socket.id;
-        socket.broadcast.emit("broadcaster");
-        console.log(`Broadcaster ${broadcasters[room]} Connect`);
-    });
+  socket.on("watcher", (room) => {
+    socket.to(broadcasters[room]).emit("watcher", socket.id);
+  });
 
-    socket.on("watcher", (room) => {
-        socket.to(broadcasters[room]).emit("watcher", socket.id);
-    })
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
 
-    socket.on("disconnect", () => {
-        socket.to(broadcaster).emit("disconnectPeer", socket.id);
+    console.log("User disconnected");
+    const user = deleteUser(socket.id);
+    if (user) {
+      io.in(user.room).emit("notification", {
+        title: "Someone just left",
+        description: `${user.name} just left the room`,
+      });
+      io.in(user.room).emit("users", getUsers(user.room));
+    }
+  });
 
-        console.log("User disconnected");
-        const user = deleteUser(socket.id)
-        if (user) {
-            io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
-            io.in(user.room).emit('users', getUsers(user.room))
-        }
-    })
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
 
-    socket.on("offer", (id, message) => {
-        socket.to(id).emit("offer", socket.id, message);
-    });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
 
-    socket.on("answer", (id, message) => {
-      socket.to(id).emit("answer", socket.id, message);
-    });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
 
-    socket.on("candidate", (id, message) => {
-      socket.to(id).emit("candidate", socket.id, message);
-    });
+  //MESSAGE CHATTING
+  socket.on("login", ({ name, room }, callback) => {
+    console.log(name, room);
+    const { user, error } = addUser(socket.id, name, room);
+    if (error) return callback(error);
+    socket.join(user.room);
+    socket
+      .in(room)
+      .emit("notification", {
+        title: "Someone's here",
+        description: `${user.name} just entered the room`,
+      });
+    io.in(room).emit("users", getUsers(room));
+    callback();
+  });
 
-    //MESSAGE CHATTING
-    socket.on("login", ({name, room}, callback) => {
-        console.log(name, room);
-        const { user, error } = addUser(socket.id, name, room)
-        if (error) return callback(error)
-        socket.join(user.room)
-        socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
-        io.in(room).emit('users', getUsers(room))
-        callback()
-    });
+  socket.on("sendMessage", (message) => {
+    console.log("Message:", message);
+    const user = getUser(socket.id);
+    io.in(user.room).emit("message", { user: user.name, text: message });
+  });
+});
 
-    socket.on("sendMessage", message => {
-        console.log("Message:", message);
-        const user = getUser(socket.id)
-        io.in(user.room).emit('message', { user: user.name, text: message });
-    });
-})
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 8000
-
-app.get('/', (req, res) => {
-    res.send('Server is up and running')
-})
-server.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Socket Server is up and running");
+});
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
